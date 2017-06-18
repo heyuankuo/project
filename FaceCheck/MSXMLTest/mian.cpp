19b8a54c46ef4dc279db8d183843bda3281de83e
heyuankuo <1421198728@qq.com>
2017-06-19 07:26:18 +0800

    ge

--- FaceCheck/MSXMLTest/mian.cpp
+++ FaceCheck/MSXMLTest/mian.cpp
@@ -1,20 +1,22 @@
 #include <windows.h>
-#include <msxml.h>
+#include <msxml6.h>
 
 int main(int argc, char *argv[])
 {
 	::CoInitialize(NULL);
 
 	// 创建文档接口
-	IXMLDOMDocument *p_ixmldoc = NULL;
-	HRESULT hr = ::CoCreateInstance(CLSID_DOMDocument,
+	IXMLDOMDocument2 *p_ixmldoc = NULL;
+	HRESULT hr = ::CoCreateInstance(CLSID_DOMDocument60,
 									NULL,
 									CLSCTX_INPROC_SERVER,
 									IID_PPV_ARGS(&p_ixmldoc) );
 
-	wchar_t *xml = L"<A>SDF</A>";
+	wchar_t *xml = L"<?xml version='1.0' encoding='GB2312'?><A>SDF</A>";
 	short xx;
 	p_ixmldoc->loadXML( xml, &xx);
+
+	// p_ixmldoc->get_parsed();
 
 	IXMLDOMNodeList *resultList = NULL;
 	p_ixmldoc->get_childNodes(&resultList);
